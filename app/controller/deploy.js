da4b16c24f307e4c50264e6b945e9dd477a549e3
gaius <gaius@qiwenbo.com>
2018-07-25 11:01:24 +0800

    feat: add marmot-release and remove service-deploy-release

--- app/controller/deploy.js
+++ app/controller/deploy.js
@@ -3,40 +3,63 @@
 const {
   Controller,
 } = require('egg');
+const marmotRelease = require('marmot-release');
+const AgentKeepalive = require('agentkeepalive');
+const OSS = require('ali-oss');
 
 class DeployController extends Controller {
   async release() {
     const ctx = this.ctx;
 
     ctx.validate({
-      acl: { type: 'string', required: true },
+      region: { type: 'string' },
+      accessKeyId: { type: 'string' },
+      accessKeySecret: { type: 'string' },
+      bucket: { type: 'string' },
+      timeout: { type: 'number', required: false },
+      acl: { type: 'string' },
       prefix: { type: 'string', allowEmpty: true },
-      source: { type: 'string', requeied: true, format: /\.(tgz)$/ },
-      region: { type: 'string', required: true },
-      accessKeyId: { type: 'string', required: true },
-      accessKeySecret: { type: 'string', required: true },
-      bucket: { type: 'string', required: true },
+      source: { type: 'string', required: false, format: /\.(tgz)$/ },
+      sourceUrl: { type: 'string', required: false },
     });
 
     const data = ctx.request.body;
     const acl = data.acl || 'default';
     const prefix = data.prefix || '';
-    const sourceUrl = data.source;
-    const region = data.region;
+    const source = data.source || '';
+    const sourceUrl = data.sourceUrl || '';
+    const region = data.region || '';
     const accessKeyId = data.accessKeyId;
     const accessKeySecret = data.accessKeySecret;
-    const bucket = data.bucket;
+    const bucket = data.bucket || '';
+    const timeout = data.timeout || 120 * 1000;
 
     ctx.logger.info('deploy start');
 
-    const [ html, other ] = await ctx.service.deploy.uploadPackage({
-      sourceUrl,
-      prefix,
+    const urllib = require('urllib').create({
+      agent: new AgentKeepalive({
+        maxSockets: 100,
+        maxFreeSockets: 10,
+        timeout: 30000,
+        freeSocketKeepAliveTimeout: 15000, // free socket keepalive for 30 seconds
+      }),
+    });
+
+    const ossClient = new OSS({
       region,
       accessKeyId,
       accessKeySecret,
       bucket,
+      timeout,
+      urllib,
+    });
+
+    const [ html, other ] = await marmotRelease.uploadPackage({
+      source,
+      sourceUrl,
+      prefix,
       acl,
+      ossClient,
     });
 
     ctx.logger.info('deploy end');
