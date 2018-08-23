8ab315f8918142e3d53ee583e210494c56983c26
Yuheng Zhang <zhangyuheng91@gmail.com>
2018-08-24 04:14:52 +0800

    fix: db config

--- database/config.js
+++ database/config.js
@@ -1,29 +1,22 @@
 'use strict';
 
+const defaultConfig = {
+  username: 'root',
+  password: 'marmot',
+  database: 'marmot_development',
+  host: process.env.MYSQL_HOST || '127.0.0.1',
+  port: process.env.MYSQL_PORT || '3306',
+  operatorsAliases: false,
+  dialect: 'mysql',
+};
+
 module.exports = {
-  development: {
-    username: 'root',
-    password: 'marmot',
-    database: 'marmot_development',
-    host: '127.0.0.1',
-    operatorsAliases: false,
-    dialect: 'mysql',
-  },
-  test: {
-    username: 'root',
-    password: 'marmot',
+  development: defaultConfig,
+  test: Object.assign({}, defaultConfig, {
     database: 'marmot_unittest',
-    host: '127.0.0.1',
-    operatorsAliases: false,
-    dialect: 'mysql',
-  },
-  production: {
-    dialect: 'mysql',
-    database: 'marmot_development',
-    host: process.env.MYSQL_HOST || '127.0.0.1',
-    port: process.env.MYSQL_PORT || '3306',
-    username: 'root',
-    password: 'marmot',
-    operatorsAliases: false,
-  },
+  }),
+  production: Object.assign({}, defaultConfig, {
+    defaultConfig,
+    database: 'marmot',
+  }),
 };
